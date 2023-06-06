4b8aa7137e25fafb40a010e20b35f8a9f021c2ef
Ilias-Deh <125891153+Ilias-Deh@users.noreply.github.com>
2023-06-06 18:34:28 +0200

    Update login.js + ajout verif et redirection accueil

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,55 +1,62 @@
+const formLogin = document.getElementById("form-login")
+const formEmail = document.getElementById("email")
+const formPassword = document.getElementById("password")
+formLogin.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const loginData = {
+        email : event.target.querySelector("[name=email]").value,
+        password : event.target.querySelector("[name=password]").value,
+    };
+    const chargeUtile = JSON.stringify(loginData);
+    if (verifEmail(formEmail) && verifPassword(formPassword)){
+        submit(chargeUtile)
+    }
 
-    const formLogin = document.getElementById("form-login")
-    const formEmail = document.getElementById("email")
-    const formPassword = document.getElementById("password")
-    formLogin.addEventListener("submit", function (event) {
+    });
+    
+async function submit(chargeUtile) {
 
-        event.preventDefault();
-        const loginData = {
-            email : event.target.querySelector("[name=email]").value,
-            password : event.target.querySelector("[name=password]").value,
-        };
+let answer = await fetch('http://localhost:5678/api/users/login', {
 
-        const chargeUtile = JSON.stringify(loginData);
+method: "POST",
+headers: { "Content-Type": "application/json" },
+body: chargeUtile
 
+})
+let data = await answer.status;
+if (data == 200) {
+    document.location.href="index.html";
 
-        if (verifEmail(formEmail) & verifPassword(formPassword) == true){
-            const answer = fetch('http://localhost:5678/api/users/login', {
-
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: chargeUtile
-            });
-        }
-        else {
-            console.log("retry")
-        }
-       
-        });
-        
-
+}
+}
 
 function verifEmail(email) {
 
-    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+")
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\\.[a-z0-9._-]+")
     if (emailRegExp.test(email.value)) {
         return true
     }
     else {
         console.log("erreur email")
+        return false
     }
 
 }
 
 function verifPassword(password) {
-    let passwordRegExp = new RegExp("[a-z0-9._-]+")
+    let passwordRegExp = new RegExp("[a-zA-Z0-9._-]+")
     if (passwordRegExp.test(password.value)) {
         return true
     }
     else {
         console.log("erreur mdp")
+        return false
     }
 }
+
+
+
+
 
 
 
